b11e5996fcbf21be9ae365bf450c15f093ac38e5
Hamza Maach <hamzamaach56@gmail.com>
2025-09-04 15:22:09 +0100

    use record instead of class in CreateUserCommand

--- src/main/java/com/letsplay/application/dto/request/CreateUserCommand.java
+++ src/main/java/com/letsplay/application/dto/request/CreateUserCommand.java
@@ -1,25 +1,7 @@
 package com.letsplay.application.dto.request;
 
-public class CreateUserCommand {
-    private final String username;
-    private final String email;
-    private final String password;
-
-    public CreateUserCommand(String username, String email, String password) {
-        this.username = username;
-        this.email = email;
-        this.password = password;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-}
+public record CreateUserCommand(
+    String username,
+    String email,
+    String password
+) {}
