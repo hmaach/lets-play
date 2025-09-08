b9517bc318ded6ff18102f4443ba04a8cfc1c017
Hamza Maach <hamzamaach56@gmail.com>
2025-09-08 15:56:50 +0100

    BCrypt the password

--- src/main/java/com/letsplay/infrastructure/config/SecurityConfig.java
+++ src/main/java/com/letsplay/infrastructure/config/SecurityConfig.java
@@ -10,7 +10,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import com.letsplay.infrastructure.security.CustomAuthHandlers;
@@ -59,7 +59,7 @@ public class SecurityConfig {
     @Bean
     public AuthenticationProvider authenticationProvider() {
         DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-        provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+        provider.setPasswordEncoder(new BCryptPasswordEncoder(10));
         provider.setUserDetailsService(userDetailsService);
         return provider;
     }
