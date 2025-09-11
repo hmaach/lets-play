acbba003f91aa20ca7ac5a39cc1f56bfe8f82eaf
Hamza Maach <hamzamaach56@gmail.com>
2025-09-11 15:34:01 +0100

    add CORS configuration

--- src/main/java/com/letsplay/infrastructure/config/CorsConfig.java
+++ src/main/java/com/letsplay/infrastructure/config/CorsConfig.java
@@ -1,5 +1,27 @@
 package com.letsplay.infrastructure.config;
 
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
 public class CorsConfig {
 
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowedOrigins(List.of("http://127.0.0.1:5500"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return source;
+    }
 }
